2d838b1fda9936f37d5d4ec152f4de674a94e0ab
June Shaw <june.shaw@me.com>
2016-02-22 12:51:05 -0700

    fixed relative date

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var app = angular.module("RedditClone", []);
+var app = angular.module("RedditClone", ['angularMoment']);
 
 app.controller("MyController", function($scope) {
   $scope.title="Reddit Clone";
   $scope.selections=[{field: "votes", descend: true}, {field: "date", descend: true}, {field: "title", descend: false}];
 
   // $scope.mirror="Mirror"
-  $scope.sites=[{title: "Cliffs of Moher", image:"http://i.imgur.com/Mycvbzf.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, leo vitae gravida dapibus, lacus neque faucibus erat, vitae pretium justo lorem sit amet lorem. Ut at diam ante. Praesent a libero consectetur, sollicitudin eros nec, ullamcorper nisi. Proin volutpat urna id semper malesuada. Nulla non ultricies dolor, vel pharetra est. Nulla neque nibh, pulvinar pretium condimentum sit amet, hendrerit sit amet neque.", votes: 4, author: "Sunny Francis", date: "2016-01-20T15:58:58.909Z", comments: [{author: "June", text: "Loved it!", date: "2016-02-21T15:58:58.909Z"}]},
+  $scope.sites=[{title: "Cliffs of Moher", image:"http://i.imgur.com/Mycvbzf.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, leo vitae gravida dapibus, lacus neque faucibus erat, vitae pretium justo lorem sit amet lorem. Ut at diam ante. Praesent a libero consectetur, sollicitudin eros nec, ullamcorper nisi. Proin volutpat urna id semper malesuada. Nulla non ultricies dolor, vel pharetra est. Nulla neque nibh, pulvinar pretium condimentum sit amet, hendrerit sit amet neque.", votes: 4, author: "Sunny Francis", date: "2016-02-20T15:58:58.909Z", comments: [{author: "June", text: "Loved it!", date: "2016-02-21T15:58:58.909Z"}]},
   {title: "Mount Rushmore", image:"http://f.tqn.com/y/americanhistory/1/W/Q/E/mountrushmore.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis, leo vitae gravida dapibus, lacus neque faucibus erat, vitae pretium justo lorem sit amet lorem. Ut at diam ante. Praesent a libero consectetur, sollicitudin eros nec, ullamcorper nisi. Proin volutpat urna id semper malesuada. Nulla non ultricies dolor, vel pharetra est. Nulla neque nibh, pulvinar pretium condimentum sit amet, hendrerit sit amet neque.", votes: 5, author: "Bonnie Murphy", date: "2016-01-20T15:58:58.909Z", comments: [{author: "Kip Meguire", text: "Loved it!", date: "2016-02-21T15:58:58.909Z"}]}]
 
   $scope.addComment = function(site, author, text) {
